a6a6ac4ba0c7a20cd0858cb6504202c41a767fbc
Rasenkai <rasenkai99@gmail.com>
2020-11-04 11:28:32 +0530

    print all odd numbers from 1 to n (no if)
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- oddprinter.c
+++ oddprinter.c
@@ -9,12 +9,9 @@ int main()
 
     printf("Odd numbers from 1 to %d are: \n", n);
 
-    for(i=1; i<=n; i++)
+    for(i=1; i<=n; i+=2)
     {
-        if(i%2!=0)
-        {
-            printf("%d\n", i);
-        }
+        printf("%d\n", i);
     }
 
     return 0;
